a0fddcaf322623799c412ae666524250537fb02e
Elender <elenderg@gmail.com>
2019-10-14 11:51:12 -0300

    Update GerenciaAeronaves.js

--- js/GerenciaAeronaves.js
+++ js/GerenciaAeronaves.js
@@ -3,7 +3,9 @@ Aeronave = [];
 meuStorage = localStorage;
 var Hora = "";
 
-function CriarAeronave(Matricula, Origem, Destino, Hora,  Nivel, Tipo, Esteira, Transponder, Procedimento, Distancia, Rota, Movimento, Coordenadas){
+function CriarAeronave(Matricula, Origem, Destino, Hora,  Nivel, Tipo, Esteira, Transponder,
+  Procedimento, Distancia, Rota, Movimento, Coordenadas, velocidade, tipodevelocidade, tipodenivel)
+{
   this.Matricula = Matricula;
   this.Origem = Origem;
   this.Destino = Destino;
@@ -16,6 +18,10 @@ function CriarAeronave(Matricula, Origem, Destino, Hora,  Nivel, Tipo, Esteira, 
   this.Procedimento = Procedimento;
   this.Rota = Rota;
   this.Movimento = Movimento;
+  this.velocidade = velocidade;
+  this.Coordenadas = Coordenadas;
+  this.tipodevelocidade = tipodevelocidade;
+  this.tipodenivel = tipodenivel;
 }
 
 function AdicionarAeronave(){
@@ -32,14 +38,17 @@ function AdicionarAeronave(){
     Procedimento = document.getElementById('procedimento').value;
     Rota = document.getElementById('rota').value.toUpperCase();
     Coordenadas = document.getElementById('coordenadas').value.toUpperCase();
-    console.log("Matrícula: " +Matricula +"; Origem: " +Origem +"; Destino: " +Destino);
+    velocidade = document.getElementById('velocidade').value.toUpperCase();
+    tipodevelocidade = document.getElementById('tipodevelocidade').value.toUpperCase();
+    tipodenivel  = document.getElementById('tipodenivel').value.toUpperCase();
+    /*console.log("Matrícula: " +Matricula +"; Origem: " +Origem +"; Destino: " +Destino);
     console.log("Hora: " +Hora +"; Nivel: " +Nivel +"; Tipo: " +Tipo + "; Distância: " + Distancia);
-    console.log("Esteira: " +Esteira +"; Transponder: " + Transponder + "; Procedimento: " + Procedimento + "; Rota: " + Rota);    
+    console.log("Esteira: " +Esteira +"; Transponder: " + Transponder + "; Procedimento: " + Procedimento + "; Rota: " + Rota);    */
     if (Matricula && Origem && Destino && Hora) {
       if(Origem !== "SBHT" && Destino !== "SBHT" && Movimento != "QAY"){
         alert("Por favor, altere o valor Movimento para QAY ou QAF")
       }
-      else {AdicionaAeronave(Matricula, Origem, Destino, Hora,  Nivel, Tipo, Esteira, Transponder, Procedimento, Rota, Movimento, Coordenadas)}
+      else {AdicionaAeronave(Matricula, Origem, Destino, Hora,  Nivel, Tipo, Esteira, Transponder, Procedimento, Rota, Movimento, Coordenadas, velocidade, tipodevelocidade, tipodenivel)}
     }
     else {
         console.log("Como os dados da aeronave não foram preeenchidos, o comando foi ignorado")    
@@ -50,16 +59,16 @@ function AdicionarAeronave(){
 }
 
 
-function AdicionaAeronave(Matricula, Origem, Destino, Hora,  Nivel, Tipo, Esteira, Transponder, Procedimento, Rota, Distancia, Movimento){
+function AdicionaAeronave(Matricula, Origem, Destino, Hora,  Nivel, Tipo, Esteira, Transponder, Procedimento, Rota, Distancia, Movimento, coordenadas, velocidade,tipodevelocidade, tipodenivel){
     // Adiciona os valores da aeronave à  variável dados
-    Aeronave[QtAeronave] = new CriarAeronave(Matricula, Origem, Destino, Hora,  Nivel, Tipo, Esteira, Transponder, Procedimento, Rota, Distancia, Movimento);
+    Aeronave[QtAeronave] = new CriarAeronave(Matricula, Origem, Destino, Hora,  Nivel, Tipo, Esteira, Transponder, Procedimento, Rota, Distancia, Movimento, coordenadas, velocidade,tipodevelocidade, tipodenivel);
     //console.log(Aeronave[QtAeronave].matricula.length);    
     console.log("Se esta mensagem foi exibida, provavelmente os dados foram adicionados com sucesso")
     console.log(Aeronave[QtAeronave]);
     console.log(Aeronave[QtAeronave].Matricula);
     console.log("Se você viu a aeronave adicionada acima, é porque deu tudo certo");
     QtAeronave = QtAeronave + 1;
-    console.log("Você já adicionou " +QtAeronave + " aeronaves ao sistema");  
+    console.log("Você já adicionou " + QtAeronave + " aeronaves ao sistema");  
     console.log(Origem, Destino);
     AdicionaCidade(Origem,Destino);
     //CriaRota(Aeronave,Origem,Destino)
